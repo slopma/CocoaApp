dbc7d14b1ddda34c4b81c1d1378c01f64c781f7e
niosto <nicolasospinat@gmail.com>
2025-09-06 22:42:10 -0500

    Add API endpoints and example files

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from routers import users, metrics
+import json
 
 app = FastAPI(title="My App API")
 
@@ -10,3 +11,17 @@ app.include_router(metrics.router, prefix="/metrics", tags=["Metrics"])
 @app.get("/")
 def root():
     return {"message": "Backend running 🚀"}
+
+@app.get("/mapa")
+def mapa():
+    with open("files/lotes.geojson","r") as f:
+        mapa = json.load(f)
+
+    return mapa
+
+@app.get("/mediciones")
+def mapa():
+    with open("files/readings.geojson","r") as f:
+        mediciones = json.load(f)
+        
+    return mediciones
